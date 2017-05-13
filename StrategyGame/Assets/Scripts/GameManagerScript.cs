08693530bcf56f696a76210196f464da21a0e3d1
MooseMadness <ggg12345jjj@gmail.com>
2017-05-13 10:53:09 +0300

    Create StrategyGame

---
+++ StrategyGame/Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public int soldierCount;
+    public int cannonCount;
+
+    public static GameManagerScript instance;
+
+    private static bool isGameEnd = false;
+
+    private float timer = 0;
+
+    private void Start()
+    {
+        instance = this;
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+    }
+
+    public void GameOver()
+    {
+        if (!isGameEnd)
+        {
+            isGameEnd = true;
+            int score = (int)(0.1f * soldierCount) + 100 * cannonCount;
+            string jsCode = @"var token = sessionStorage.access_token;
+                        var xmlhttp = new XMLHttpRequest(); 
+                        xmlhttp.open('POST', 'https://api.sagaidachniepath.xyz/quests/checkpoint');
+                        xmlhttp.setRequestHeader('Content-Type', 'application/json');
+                        xmlhttp.setRequestHeader('authorization', 'Bearer ' + token);
+                        xmlhttp.onreadystatechange = function() {
+                            if (xmlhttp.readyState == 4) {
+                                if(xmlhttp.status == 204) {
+                                    window.location = 'https://sagaidachniepath.xyz/quest';
+                                } 
+                            }
+                        };
+                        xmlhttp.send(JSON.stringify({ points: '" + score + "', elapsedTime: '" + timer * 1000 + "' }));";
+            Application.ExternalEval(jsCode);
+            SceneManager.LoadScene("Scenes/WaitScene");
+        }
+    }
+
+    public void AddSoldier(int amount)
+    {
+        soldierCount += amount;
+        if (soldierCount < 0)
+            soldierCount = 0;
+    }
+
+    public void AddCannon(int amount)
+    {
+        cannonCount += amount;
+        if (cannonCount < 0)
+            cannonCount = 0;
+    }
+}
---
+++ StrategyGame/Assets/Scripts/QuestionScreenScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestionScreenScript : MonoBehaviour
+{
+    public Text armyStateText;
+
+    public int gaSoldierCost;
+    public int baSoldierCost;
+    public int gaCannonCost;
+    public int baCannonCost;
+
+    public void OnGoodAnswer()
+    {
+        GameManagerScript.instance.AddSoldier(gaSoldierCost);
+        GameManagerScript.instance.AddCannon(gaCannonCost);
+    }
+
+    public void OnBadAnswer()
+    {
+        GameManagerScript.instance.AddSoldier(baSoldierCost);
+        GameManagerScript.instance.AddCannon(baCannonCost);
+    }
+
+    public void Setup()
+    {
+        armyStateText.text = string.Format("{0} козаків\n{1} гармат", GameManagerScript.instance.soldierCount, GameManagerScript.instance.cannonCount);
+    }
+}
